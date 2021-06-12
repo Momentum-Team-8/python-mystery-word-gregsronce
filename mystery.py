bd5cb18083765a7d4ad0d7af60c0b901fbd07199
Greg Sronce <greg.sronce@gmail.com>
2021-06-12 16:44:16 -0400

    Able to add guessed letter.

--- mystery.py
+++ mystery.py
@@ -1,24 +1,48 @@
-from hashlib import new
 import random
 
 with open("words.txt") as words_txt:
     word_options = words_txt.read()
 
 def gamePrompt():
-    split_words = list(map(str, word_options.split()))
-    randomize_word = (random.choice(split_words))
-    # random_word = "carrot"
-    print(randomize_word)
-    split_letters = list(randomize_word)
-    word_length = (len(split_letters))
-    print(word_length)
-    # word_selection = "_" * len(randomize_word)
-    # print (word_selection)
-    # input_letter = input("Make a guess: ")
-    # print(input_letter)
-    # dict = {}
-    # dict["c"] = 1
-    # print(dict)
+    # split_words = list(map(str, word_options.split()))
+    # split_letters = list(random_word)
+    # print(split_letters)
+    # tries = 0
+    # while tries > 10:
+    # word_selection = "_" * word_length
+    # split_words = random_words.split(' ')
+    number_of_guesses = 8
+    random_words = ["carrot", "goal"]
+    random_word = random.choice(random_words)
+    word_length = (len(random_word))
+    guesses = []
+   
+        for letter in random_word:
+            guesses.append('_')
+        input_letter = str(input("Make a guess: "))
+        if input_letter in random_word:
+            print("Correct")
+            for letter in range(word_length):
+                if input_letter == random_word[letter]:
+                    guesses[letter] = random_word[letter]
+                    print(guesses)
+        else: 
+            print("Sorry, guess again")
+            number_of_guesses -= 1 
+        input_letter = input_letter.strip()
+
+
+    
+    # guesses += input_letter 
+    # print(guesses)
+    # print(type(input_letter))
+
+    # if the input letter number corresponds to the space number, then replace space with input letter. 
+        
+    # letter_list[0].replace("_", input_letter)
+    # # word_selection[0] += input_letter
+    # print(letter_list)
+
 
     # for letter of words:
         # print(randomword)
